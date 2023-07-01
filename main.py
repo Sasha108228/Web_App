0d999c490d7af4315bc1caa7b307105334c4fb23
Sasha <69010894+Sasha108228@users.noreply.github.com>
2023-07-01 14:58:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+
+bot = Bot("6168841421:AAF1Te0tegtkqBc9hV_yxuHymJMG8YHmoHQ")
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+	markup = types.ReplyKeyboardMarkup()
+	markup.add(types.KeyboardButton('open web-app', web_app=WebAppInfo(url='https://itproger.com')))
+	await message.answer('Hi', reply_markup=markup)
+
+
+
+executor.start_polling(dp)
